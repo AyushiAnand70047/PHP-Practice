c81b0e56cf47c800b3599514df664878b1a10a98
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-04 09:54:00 +0530

    php practice

---
+++ Day6/basic-functions.php
@@ -0,0 +1,21 @@
+<?php
+$str = "Ayushi Anand";
+echo strlen($str)."<br/>";
+$var = false;
+var_dump($var);
+echo "<br/>";
+echo date("M");
+echo "<br/>";
+$check = "abc";
+echo is_string($check);
+echo "<br/>";
+echo rand();
+echo "<br/>";
+$st = "code step by step";
+echo substr($st,5);
+echo "<br/>";
+echo substr($st,-5);
+// die;
+exit;
+echo phpinfo();
+?>
