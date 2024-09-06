31deb349f63df3084c230bcc668d12060494666c
AyushiAnand70047 <ayushianand088@gmail.com>
2024-09-06 11:07:00 +0530

    loops and string function practice

---
+++ Day7/loops/count-word-in-string.php
@@ -0,0 +1,5 @@
+<?php
+$my_str = 'The quick brown fox jumps over the lazy dog.';
+ // Calculating and displaying number of words
+echo str_word_count($my_str);
+?>
---
+++ Day7/loops/replace-text-in-string.php
@@ -0,0 +1,5 @@
+<?php
+$my_str = 'If the facts do not fit the theory, change the facts.';
+ // Display replaced string
+echo str_replace("facts", "truth", $my_str);
+?>
