9f4182c37868802849028acebea1d9048d8160b0
AyushiAnand70047 <ayushianand088@gmail.com>
2024-10-09 10:36:53 +0530

    send data from html to php using post request

--- Day8/HTML interact with PHP/login.php
+++ Day8/HTML interact with PHP/login.php
@@ -1,6 +1,14 @@
 <?php
-if($_GET){
-    echo "User name is ".$_GET['user_name']."<br/>";
-    echo "User password is ".$_GET['user_password']."<br/>";
-}
+
+// get request
+// if($_GET){
+//     echo "User name is ".$_GET['user_name']."<br/>";
+//     echo "User password is ".$_GET['user_password'];
+// }
+
+// post request
+print_r($_POST);
+echo "<br/>";
+echo "User name is ".$_POST['user_name']."<br/>";
+echo "User password id ".$_POST['user_password'];
 ?>
