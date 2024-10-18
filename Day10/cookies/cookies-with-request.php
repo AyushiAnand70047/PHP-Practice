f9d37dc5ac5489f17df1561f4da94a63c2dc9202
AyushiAnand70047 <ayushianand088@gmail.com>
2024-10-18 14:55:09 +0530

    cookies with input filed

---
+++ Day10/cookies/cookies-with-request.php
@@ -0,0 +1,27 @@
+<form action="" method="post">
+    <input type="text" name="user" placeholder="enter your name">
+    <br/><br/>
+    <button name="btn" value="set">Set Cookies</button>
+    <button name="btn" value="display">Display Cookies</button>
+    <button name="btn" value="delete">Delete Cookies</button>
+</form>
+
+<?php
+if(isset($_POST['btn'])){
+    $val = $_POST['user'];
+    if($_POST['btn'] == 'set'){
+        setcookie("user",$val);
+        echo "Cookie is set";
+    }
+    if($_POST['btn'] == 'display'){
+        if(isset($_COOKIE['user'])){
+            echo $_COOKIE['user'];
+        }
+    }
+    if($_POST['btn'] == 'delete'){
+        if(isset($_COOKIE['user'])){
+           setcookie("user",null,-1); 
+        }
+    }
+}
+?>
